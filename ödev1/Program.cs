e86e8f6284d8ffe38bac697cfa7c6fd21fa9cc25
Semiha Özdemir <semihaozdemir34@gmail.com>
2021-11-24 19:05:28 +0300

    Add project files.

---
+++ ödev1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ödev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  1) kullanıcıdan tam sayı değer istenecek.sayı pozitifse sayı pozitif mesajı, negatifse negatif mesajı, 
+            // 0 ise sayı 0'dır mesajı ekranda gösterilecek...
+
+            Console.WriteLine("lütfen bir tam sayı değeri giriniz.");
+            int sayi = int.Parse(Console.ReadLine());
+
+
+
+            if (sayi > 0)
+            {
+                Console.WriteLine("girilen sayı pozitif değerdedir.");
+            }
+
+            else if (sayi < 0)
+            {
+                Console.WriteLine("girilen sayı negatif değerdedir.");
+            }
+            else 
+            {
+                Console.WriteLine("girdiğiniz sayı sıfırdır.lütfen tekrar deneyiniz.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ödev2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ödev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 2 ) kullanıcında vize ve final notları istenecek. alınan vize ve finalden ortalama hesaplanacak.
+                 Vizenin 0.4'ü finalin 0.6'sı.ve ortalması 100 ile 80 arasında ise AA,80 ve 70 arasında ise BA 
+                 diğer değer aralıklarını belirleyerek ekrana harf notunu gösteren mesajı veren prog. yazınız..*/
+
+            Console.WriteLine("lütfen vize notunuzu giriniz.");
+            int vizenotu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("lütfen final notunuzu giriniz.");
+            int finalnotu = int.Parse(Console.ReadLine());
+
+
+            int sonuc=(((vizenotu*40)/100)+((finalnotu*60)/100));
+
+            if (sonuc < 100 && sonuc > 80)
+            {
+                Console.WriteLine("not ortalamanız AA'dır");
+            }
+            else if (sonuc < 80 && sonuc > 70)
+            {
+                Console.WriteLine("not ortalamanız BA'dır.");
+            }
+            else if (sonuc < 70 && sonuc > 50)
+            {
+                Console.WriteLine("not ortalamanız BB'dir.");
+
+            }
+            else if (sonuc < 50 && sonuc > 30)
+            {
+                Console.WriteLine("not ortalamanız CC'dir");
+            }
+            else if (sonuc < 30 && sonuc > 10) 
+            {
+                Console.WriteLine("not ortalamanız DD'dir.");
+            }
+
+            else
+            {
+                Console.WriteLine("not ortalamanız FF'dir.");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
